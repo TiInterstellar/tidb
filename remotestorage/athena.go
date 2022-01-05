a27b5d9a9a6fe4e59e5f9ffb1fa43a351e7cd657
Xuanwo <github@xuanwo.io>
2022-01-05 16:56:24 +0800

    Add more comment
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ remotestorage/athena.go
@@ -0,0 +1,35 @@
+package remotestorage
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/athena"
+	"github.com/aws/aws-sdk-go/service/glue"
+)
+
+type Athena struct {
+	glue   *glue.Glue
+	client *athena.Athena
+}
+
+func (a *Athena) Setup() {
+	// 1. Create a glue database
+	_, _ = a.glue.CreateDatabase(nil)
+
+	// 2. Create a glue table with pre-defined schema
+	_, _ = a.glue.CreateTable(nil)
+
+	// 3. Create a glue crawler to crawl the table data from s3
+	_, _ = a.glue.CreateCrawler(nil)
+
+	// 4. Create a glue job to run the crawler
+	_, _ = a.glue.CreateJob(nil)
+
+	// 5. Create a athena catalog with glue
+	_, _ = a.client.CreateDataCatalog(&athena.CreateDataCatalogInput{
+		Name: aws.String("implement-me"),
+		Type: aws.String("glue"),
+		Parameters: map[string]*string{
+			"catalog_id": aws.String("implement-me"),
+		},
+	})
+}
--- remotestorage/storage.go
+++ remotestorage/storage.go
@@ -1,3 +1,11 @@
 package remotestorage
 
-// TODO: implement csv/parquet writer
+// The whole picture of write data into s3.
+//
+// 1. Create database in Glue (maybe the same name with tidb?)
+// 2. Fetch the table schema, and create the linking table in Glue.
+// 3. Setup crawler to allow Glue fetch data from the s3.
+// 4. Create catalog at Athena side to load from Glue.
+// 5. Write data (in csv or parquet) into s3 and trigger the Glue crawler.
+//
+// After all these setups, we can read data from Athena now.
