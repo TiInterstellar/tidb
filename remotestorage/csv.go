665110930371f4fc714ecc8ee1b477caff7970cb
Xuanwo <github@xuanwo.io>
2022-01-05 18:17:06 +0800

    Use hardcoded size limit
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- remotestorage/csv.go
+++ remotestorage/csv.go
@@ -7,12 +7,11 @@ import (
 	"sync"
 )
 
-// This a hard coded limit of records.
-// If per-record is 64B, we have a 64M file.
-const recordLimit = 1024 * 1024
+// This a hard coded size limit of records, we use 64MiB here.
+const sizeLimit = 64 * 1024 * 1024
 
 var csvPool = sync.Pool{New: func() interface{} {
-	return bytes.NewBuffer(make([]byte, 0, 64*recordLimit))
+	return bytes.NewBuffer(make([]byte, 0, sizeLimit))
 }}
 
 // CsvWriter will write data into memory and then persist into s3.
@@ -22,7 +21,6 @@ type CsvWriter struct {
 	store *S3
 	path  string // path id for this csv.
 
-	count  uint // Count for the current csv.
 	buf    *bytes.Buffer
 	writer *csv.Writer
 }
@@ -42,8 +40,10 @@ func NewCsvWriter(store *S3, path string) *CsvWriter {
 }
 
 // IsFree must be called before calling WriteRecord.
+//
+// It's OK to write a bit more data than the size limit.
 func (c *CsvWriter) IsFree() bool {
-	return c.count <= recordLimit
+	return c.buf.Len() < sizeLimit
 }
 
 func (c *CsvWriter) WriteRecord(record []string) (err error) {
@@ -51,8 +51,6 @@ func (c *CsvWriter) WriteRecord(record []string) (err error) {
 	if err != nil {
 		return fmt.Errorf("write csv record: %w", err)
 	}
-
-	c.count++
 	return nil
 }
 
